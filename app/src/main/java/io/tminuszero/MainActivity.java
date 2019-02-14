fb37fccfde3a26750d878baaaf9c31d013a55659
DJAuxel <alexnlt9@gmail.com>
2019-02-13 20:36:21 -0800

    Initial commit

---
+++ app/src/main/java/io/tminuszero/MainActivity.java
@@ -0,0 +1,48 @@
+package io.tminuszero;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new DashboardFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+
+            switch(menuItem.getItemId()) {
+                case R.id.nav_dashboard:
+                    selectedFragment = new DashboardFragment();
+                    break;
+                case R.id.nav_launch_schedule:
+                    selectedFragment = new LaunchScheduleFragment();
+                    break;
+                case R.id.nav_rockets:
+                    selectedFragment = new RocketsFragment();
+                    break;
+                case R.id.nav_settings:
+                    selectedFragment = new SettingsFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+            return true;
+        }
+    };
+}
