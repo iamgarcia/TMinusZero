f8638f2ef6781a3c2de7b9262104f035b6401c79
Ace <ace.g.fig@gmail.com>
2019-04-27 23:12:22 -0700

    Refactored SQL_CREATE_ENTRIES

--- app/src/main/java/io/tminuszero/db/DataBaseHelper.java
+++ app/src/main/java/io/tminuszero/db/DataBaseHelper.java
@@ -10,14 +10,18 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "TMinusZero.db";
 
+
     private static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE " + DataBaseContract.DBEntry.TABLE_NAME
+            "CREATE TABLE " + DataBaseContract.DBEntry.LAUNCH_TABLE
             + " (" + DataBaseContract.DBEntry._ID + " INTEGER PRIMARY KEY," +
                     DataBaseContract.DBEntry.COLUMN_NAME_LAUNCH + " TEXT," +
-                    DataBaseContract.DBEntry.COLUMN_NET_LAUNCH + " TEXT)";
+                    DataBaseContract.DBEntry.COLUMN_NET_LAUNCH + " TEXT," +
+                    DataBaseContract.DBEntry.COLUMN_TBDTIME_LAUNCH + " INTEGER," +
+                    DataBaseContract.DBEntry.COLUMN_TBDDATE_LAUNCH + " INTEGER," +
+                    DataBaseContract.DBEntry.COLUMN_PROBABILITY_LAUNCH + " INTEGER)";
 
     private static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS " + DataBaseContract.DBEntry.TABLE_NAME;
+            "DROP TABLE IF EXISTS " + DataBaseContract.DBEntry.LAUNCH_TABLE;
 
     public DataBaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
