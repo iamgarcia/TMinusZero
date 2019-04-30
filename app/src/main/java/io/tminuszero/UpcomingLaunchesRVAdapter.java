9f719e625ae7671ee0cd5de42c944004dfc883e5
Alexander Garcia <alexnlt9@gmail.com>
2019-04-30 14:12:17 -0700

    onClickListener for launch items is working.

--- app/src/main/java/io/tminuszero/UpcomingLaunchesRVAdapter.java
+++ app/src/main/java/io/tminuszero/UpcomingLaunchesRVAdapter.java
@@ -1,11 +1,17 @@
 package io.tminuszero;
 
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -30,9 +36,7 @@ public class UpcomingLaunchesRVAdapter extends RecyclerView.Adapter<UpcomingLaun
     @Override
     public void onBindViewHolder(LaunchViewHolder launchViewHolder, int i) {
         Launch launch = upcomingLaunchList.get(i);
-
         launchViewHolder.launchSequence.setText(String.valueOf(launch.getSequence()));
-//        launchViewHolder.launchServiceProvider.setText(launch.lsp.getName());
         launchViewHolder.launchVehicle.setText(launch.rocket.getName());
         launchViewHolder.missionName.setText(launch.mission.getName());
     }
@@ -43,17 +47,27 @@ public class UpcomingLaunchesRVAdapter extends RecyclerView.Adapter<UpcomingLaun
     }
 
     public static class LaunchViewHolder extends RecyclerView.ViewHolder {
+
         CardView mCardView;
         TextView launchSequence;
         TextView launchVehicle;
         TextView missionName;
 
-        public LaunchViewHolder(View itemView) {
+        public LaunchViewHolder(final View itemView) {
             super(itemView);
             mCardView = itemView.findViewById(R.id.card_view_launches);
             launchSequence = itemView.findViewById(R.id.launch_sequence);
             launchVehicle = itemView.findViewById(R.id.launch_vehicle);
             missionName = itemView.findViewById(R.id.mission_name);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AppCompatActivity activity = (AppCompatActivity) v.getContext();
+                    Fragment myFragment = new LaunchesItemView();
+                    activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, myFragment).addToBackStack(null).commit();
+                }
+            });
         }
 
     }
